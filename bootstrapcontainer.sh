020c40d382ca46fe30ccec8417450a0c3cdb114e
Akshay Joseph <kmjakshay@gmail.com>
2021-05-19 11:15:35 +0530

    better comments

--- bootstrapcontainer.sh
+++ bootstrapcontainer.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+#Make a local temp dir
 mkdir adjust
+#Clone the sample http app to the temp dir
 git clone https://github.com/sawasy/http_server adjust
-
+#docker build the container
 docker build -t rubyhttps:latest .
-
+#remove temp dir
 rm -fr adjust
