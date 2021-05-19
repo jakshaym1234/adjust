f7a195e23800ab3ceb8b06e2ab4e842473044fd9
Akshay Joseph <kmjakshay@gmail.com>
2021-05-19 11:30:36 +0530

    added docker push to docker hub

--- bootstrapcontainer.sh
+++ bootstrapcontainer.sh
@@ -5,6 +5,9 @@ mkdir adjust
 #Clone the sample http app to the temp dir
 git clone https://github.com/sawasy/http_server adjust
 #docker build the container
-docker build -t rubyhttps:latest .
+docker build -t jakshaym/rubyhttps:latest .
 #remove temp dir
 rm -fr adjust
+#push the container to registry username password from env variables for security
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker push jakshaym/rubyhttps:latest
